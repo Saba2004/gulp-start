c9a0a0804125a1a182206207dcf523d4dc01de3d
Nikita <Sabaev-nik29@yandex.ru>
2024-08-09 19:30:24 +0300

    commit js + reviews
    промежуточный коммит перед отправкой на проверку

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,34 @@
+const menu = document.querySelector('.main-nav__menu')
+const buttonClose = document.querySelector('.main-nav__close')
+const buttonOpen = document.querySelector('.main-nav__burger')
+
+const closeMenu = () => {
+    menu.classList.remove('main-nav__menu--showed')
+    buttonClose.removeEventListener('click',closeMenu)
+}
+
+
+buttonOpen.addEventListener('click',() => {
+    menu.classList.add('main-nav__menu--showed')
+    buttonClose.addEventListener('click',closeMenu)
+})
+
+const btnModal = document.querySelectorAll('*[data-modal-btn]')
+
+btnModal.forEach((button) => {
+    button.addEventListener('click',function(){
+        let name = button.getAttribute('data-modal-btn')
+        let modal = document.querySelector("[data-modal-product='"+name+"']")
+        modal.style.display = "block"
+        let close = modal.querySelector('.modal__close')
+        let butContinue = document.querySelector('.modal__button')
+        butContinue.addEventListener('click',function(){
+            modal.style.display = "none"
+        })
+        close.addEventListener('click',function(){
+            modal.style.display = "none"
+        })
+    })
+    
+})
 
