adde9a5780e72a04ba25b3c4a22192bf2be76db5
Nikita <Sabaev-nik29@yandex.ru>
2024-08-26 12:03:52 +0300

    js2 on check

---
+++ source/js/products.js
@@ -0,0 +1,26 @@
+export default [    
+    {
+        id: 1,
+        name: 'Slick formal sneaker shoe',
+        image: './img/catalog-selling1.png',
+        price_new: '2 999 ₽',
+        price_old: '4999 ₽',
+        status: 'hot',
+        size: 'big',
+    },
+    {
+        id: 2,
+        name: 'Slick sneaker shoe',
+        image: './img/catalog-selling2.png',
+        price_new: '2 999 ₽',
+        price_old: '3999 ₽',
+        status: 'new',
+    },
+    {
+        id: 3,
+        name: 'Slick canvas shoe',
+        image: '../img/catalog-selling3.png',
+        price_new: '2 999 ₽',
+        price_old: '3999 ₽',
+    },
+];
